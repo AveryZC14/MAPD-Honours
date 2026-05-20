181ed1776d94ce3dde4c74a47148121d913f09ed
Avery Cheng <averyzc14@gmail.com>
2026-05-20 18:21:24 +1000

    add new lgf files

--- map_reduction_test/LGFtoGEXF.py
+++ map_reduction_test/LGFtoGEXF.py
@@ -7,8 +7,10 @@ from xml.dom import minidom
 # INPUT_FILE = './../instances/warehouseLarge/warehouseLarge_16000.lgf'
 # INPUT_FILE = './../instances/random/random_400.lgf'
 INPUT_FILE = './../instances/custom/tiny/tiny.lgf'
+INPUT_FILE = './../instances/custom/tiny/tiny_c1.lgf'
 # OUTPUT_GEXF = 'visualisation/random64x64graph2.gexf'
 OUTPUT_GEXF = 'visualisation/tiny.gexf'
+OUTPUT_GEXF = 'visualisation/tiny_c1.gexf'
 COORD_MULTIPLIER = 10
 # ---------------------
 
